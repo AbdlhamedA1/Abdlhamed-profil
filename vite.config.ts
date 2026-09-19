bd9bbb6348376fea971928ef766ee17e60a24675
Abdalhamed Abdlrady <abdlhameedabdlrady@gmail.com>
2026-09-19 13:22:34 +0300

    Configure GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Abdlhamed-profil/',
+  base: '/Abdlhamed-_profil/',
   server: {
     port: 3000,
     open: false,
